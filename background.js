e8c7e4cc394d88553d99cb42937f99f664b35fdb
Jeffrey Long <jlong@groupon.com>
2014-12-11 14:32:08 -0600

    Download detected image url

--- background.js
+++ background.js
@@ -8,6 +8,12 @@ function handleClick(tab){
   chrome.tabs.executeScript(null, {file: 'content_script.js'});
 };
 
+function handleDownloadUrl(request, sender, sendResponse){
+  chrome.downloads.download({url: request.imageUrl});
+};
+
 chrome.tabs.onUpdated.addListener(checkForInstagramPhoto);
 
 chrome.pageAction.onClicked.addListener(handleClick);
+
+chrome.runtime.onMessage.addListener(handleDownloadUrl);
