03d46f61abacf937f7c11d438cdd59ced009fbf5
Jeffrey Long <jlong@groupon.com>
2014-12-11 14:32:18 -0600

    formatting & style

--- background.js
+++ background.js
@@ -2,15 +2,15 @@ function checkForInstagramPhoto(tabId, changeInfo, tab){
   if (tab.url.indexOf('http://instagram.com/p') === 0){
     chrome.pageAction.show(tabId);
   }
-};
+}
 
 function handleClick(tab){
   chrome.tabs.executeScript(null, {file: 'content_script.js'});
-};
+}
 
 function handleDownloadUrl(request, sender, sendResponse){
   chrome.downloads.download({url: request.imageUrl});
-};
+}
 
 chrome.tabs.onUpdated.addListener(checkForInstagramPhoto);
 
